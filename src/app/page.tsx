b91815e27a839c35454a7242c28b02d1f0c9b8ae
Miguel <113023936+CodewithMiguelll@users.noreply.github.com>
2025-08-23 21:37:35 +0100

    fix: refactor font imports and enhance text styling for better readability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,42 @@
-import { Pacifico, Quicksand } from "next/font/google";
+import { Quicksand, Prompt, Podkova } from "next/font/google";
 import { FlipWords } from "@/components/ui/flip-words";
 
-const pacifico = Pacifico({
-  variable: "--font-pacifico",
-  subsets: ["latin"],
-  weight: "400",
-});
-
+// Font for headings.
 const quicksand = Quicksand({
   variable: "--font-quicksand",
   subsets: ["latin"],
   weight: "400",
+});
+
+const quicksandBold = Quicksand({
+  variable: "--font-quicksand-bold",
+  subsets: ["latin"],
+  weight: "700",
+});
+
+//Font for body text.
+const prompt = Prompt({
+  variable: "--font-prompt",
+  subsets: ["latin"],
+  weight: "400",
+});
+
+const promptBold = Prompt({
+  variable: "--font-prompt-bold",
+  subsets: ["latin"],
+  weight: "700",
+});
+
+const podkova = Podkova({
+  variable: "--font-podkova",
+  subsets: ["latin"],
+  weight: "400",
+})
+
+const podkovaBold = Podkova({
+  variable: "--font-podkova",
+  subsets: ["latin"],
+  weight: "700",
 });
 
  const words = ["UI", "Product", "Website", "Vision"];
@@ -19,14 +45,14 @@ const quicksand = Quicksand({
 export default function Home() {
   return (
     <>
-      <div className="flex justify-center items-center md:items-start h-screen flex-col gap-4 p-8">
-        <h2 className={`quicksand.className font-medium text-xl md:text-3xl`}>
+      <div className="flex justify-center items-center md:items-start h-screen flex-col gap-4 p-8 text-white">
+        <h2 className={`${quicksand.className} font-medium text-xl md:text-3xl`}>
           Let's build your next
           {/*To change the color, you can do so easily by adding TW classes directly to the FlipWords component */}
-          <FlipWords words={words} />
+          <FlipWords className="text-white font-bold" words={words} />
           at
         </h2>
-        <h1 className={`quicksand.className text-3xl md:text-6xl`}>
+        <h1 className={`${quicksandBold.className} text-3xl md:text-6xl`}>
           Northlight Creative
         </h1>
       </div>
