c15997e8b09396c9213724fca19e0531d6d5a20e
Miguel <113023936+CodewithMiguelll@users.noreply.github.com>
2025-08-23 09:34:54 +0100

    fix: add comment for customizing FlipWords component color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,6 +22,7 @@ export default function Home() {
       <div className="flex justify-center items-center md:items-start h-screen flex-col gap-4 p-8">
         <h2 className={`quicksand.className font-medium text-xl md:text-3xl`}>
           Let's build your next
+          {/*To change the color, you can do so easily by adding TW classes directly to the FlipWords component */}
           <FlipWords words={words} />
           at
         </h2>
