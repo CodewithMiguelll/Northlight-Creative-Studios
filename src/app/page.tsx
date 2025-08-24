aa33584ce1ec0c0ae673e025f29572a187126825
Miguel <113023936+CodewithMiguelll@users.noreply.github.com>
2025-08-24 10:50:44 +0100

    fix: add DotLottieReact component for enhanced animation display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+"use client"
 import { Quicksand, Prompt, Podkova } from "next/font/google";
 import { FlipWords } from "@/components/ui/flip-words";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 // Font for headings.
 const quicksand = Quicksand({
@@ -31,7 +33,7 @@ const podkova = Podkova({
   variable: "--font-podkova",
   subsets: ["latin"],
   weight: "400",
-})
+});
 
 const podkovaBold = Podkova({
   variable: "--font-podkova",
@@ -39,22 +41,35 @@ const podkovaBold = Podkova({
   weight: "700",
 });
 
- const words = ["UI", "Product", "Website", "Vision"];
- 
+const words = ["UI", "Product", "Website", "Vision"];
 
 export default function Home() {
   return (
     <>
-      <div className="flex justify-center items-center md:items-start h-screen flex-col gap-4 p-8 text-white">
-        <h2 className={`${quicksand.className} font-medium text-xl md:text-3xl`}>
-          Let's build your next
-          {/*To change the color, you can do so easily by adding TW classes directly to the FlipWords component */}
-          <FlipWords className="text-white font-bold" words={words} />
-          at
-        </h2>
-        <h1 className={`${quicksandBold.className} text-3xl md:text-6xl`}>
-          Northlight Creative
-        </h1>
+      <div className="flex flex-row justify-around items-center p-4">
+        <div className="flex justify-center items-center md:items-start h-screen flex-col gap-4 p-8 text-white">
+          <h2
+            className={`${quicksand.className} font-medium text-xl md:text-3xl`}
+          >
+            Let's build your next
+            {/*To change the color, you can do so easily by adding TW classes directly to the FlipWords component */}
+            <FlipWords className="text-white font-bold" words={words} />
+            at
+          </h2>
+          <h1 className={`${quicksandBold.className} text-3xl md:text-6xl`}>
+            Northlight Creative
+          </h1>
+        </div>
+        <div
+          className="hidden md:flex justify-center items-center h-screen"
+        >
+          <DotLottieReact
+            src="https://lottie.host/f20eba1e-5aa2-464a-8c78-34c2d2d245ca/VBIdAfUkuI.lottie"
+            autoplay
+            loop
+            style={{ width: 400, height: 400 }}
+          />
+        </div>
       </div>
     </>
   );
