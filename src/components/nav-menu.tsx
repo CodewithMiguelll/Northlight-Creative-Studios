467ee788842e47fe372390741ee65185aec4780e
Miguel <113023936+CodewithMiguelll@users.noreply.github.com>
2025-08-26 20:27:58 +0100

    feat: update Navigation component styles for improved readability and hover effects

--- src/components/nav-menu.tsx
+++ src/components/nav-menu.tsx
@@ -11,26 +11,38 @@ import {
 
 const Navigation = () => {
   return (
-    <div className="flex justify-center my-6 text-white">
+    <div className="flex justify-center my-6 text-[#121212] font-medium">
       <NavigationMenu className="mx-4 flex">
         <NavigationMenuList>
           <NavigationMenuItem>
-            <NavigationMenuLink className="flex text-[16px]" href="/about">
+            <NavigationMenuLink
+              className="flex text-[16px] hover:bg-[#f3f4f6]"
+              href="/about"
+            >
               About
             </NavigationMenuLink>
           </NavigationMenuItem>
           <NavigationMenuItem>
-            <NavigationMenuLink className="flex text-[16px]" href="/features">
+            <NavigationMenuLink
+              className="flex text-[16px] hover:bg-[#f3f4f6]"
+              href="/features"
+            >
               Features
             </NavigationMenuLink>
           </NavigationMenuItem>
           <NavigationMenuItem>
-            <NavigationMenuLink className="flex text-[16px]" href="/projects">
+            <NavigationMenuLink
+              className="flex text-[16px] hover:bg-[#f3f4f6]"
+              href="/projects"
+            >
               Projects
             </NavigationMenuLink>
           </NavigationMenuItem>
           <NavigationMenuItem>
-            <NavigationMenuLink className="flex text-[16px]" href="/services">
+            <NavigationMenuLink
+              className="flex text-[16px] hover:bg-[#f3f4f6]"
+              href="/services"
+            >
               Services
             </NavigationMenuLink>
           </NavigationMenuItem>
