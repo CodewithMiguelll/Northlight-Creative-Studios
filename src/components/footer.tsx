5ff108e1df26d2df0d98dd76a8e12f791faafeac
Miguel <113023936+CodewithMiguelll@users.noreply.github.com>
2025-08-31 15:40:30 +0100

    chore(content): updated links in the footer component.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,17 +3,14 @@ import Link from "next/link";
 export const Footer = () => (
   <footer className="flex flex-col items-center justify-center p-6 border-t border-t-secondary-foreground">
     <nav className="flex space-x-4">
-      <Link href="/features" className="text-sm text-secondary-foreground underline-offset-4 hover:underline">
-        Features
-      </Link>
       <Link href="/about" className="text-sm text-secondary-foreground underline-offset-4 hover:underline">
         About
       </Link>
-      <Link href="/services" className="text-sm text-secondary-foreground underline-offset-4 hover:underline">
-        Services
-      </Link>
       <Link href="/projects" className="text-sm text-secondary-foreground underline-offset-4 hover:underline">
         Projects
+      </Link>
+      <Link href="/services" className="text-sm text-secondary-foreground underline-offset-4 hover:underline">
+        Services
       </Link>
       <Link
         href="https://github.com/CodeWithMiguelll"
