1fe3721530530ac654331066489f8d401327f4a3
Miguel <113023936+CodewithMiguelll@users.noreply.github.com>
2025-08-31 15:15:32 +0100

    feat(layout): Implemented the footer component in the layout file.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/header";
+import {Footer} from "@/components/footer";
 
 export const metadata: Metadata = {
   icons: {
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
