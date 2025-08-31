210abdee5acd2b116720d5f0e24fab94e147b0ab
Miguel <113023936+CodewithMiguelll@users.noreply.github.com>
2025-08-31 15:12:34 +0100

    feat(footer): Set up a boilerplate footer for improved navigation.

---
+++ src/components/footer.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+
+export const Footer = () => (
+  <footer className="flex flex-col items-center justify-center p-6 border-t border-t-secondary-foreground">
+    <nav className="flex space-x-4">
+      <Link href="/features" className="text-sm text-secondary-foreground underline-offset-4 hover:underline">
+        Features
+      </Link>
+      <Link href="/about" className="text-sm text-secondary-foreground underline-offset-4 hover:underline">
+        About
+      </Link>
+      <Link href="/services" className="text-sm text-secondary-foreground underline-offset-4 hover:underline">
+        Services
+      </Link>
+      <Link href="/projects" className="text-sm text-secondary-foreground underline-offset-4 hover:underline">
+        Projects
+      </Link>
+      <Link
+        href="https://github.com/CodeWithMiguelll"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-sm text-secondary-foreground underline-offset-4 hover:underline"
+      > 
+        GitHub
+      </Link>
+      <Link
+        href="https://www.instagram.com/_big.migz/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-sm text-secondary-foreground underline-offset-4 hover:underline"
+      >
+        Instagram
+      </Link>
+    </nav>
+  </footer>
+);
