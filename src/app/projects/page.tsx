a677c0575ff8298b40a452bd6854577b6ddd426a
Miguel <113023936+CodewithMiguelll@users.noreply.github.com>
2025-10-31 15:51:26 +0100

    chore(projects): remove unnecessary version number for Nextjs in project description.

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -42,7 +42,7 @@ const projects = [
   {
     title: "Rise Of The Rejects Website",
     description:
-      "Built with Next.js 15, TypeScript, and Framer Motion, this project extends my screenplay Rise of the Rejects beyond the page — into a cinematic, interactive world that fuses code, creativity, and conviction.",
+      "Built with Next.js, TypeScript, and Framer Motion, this project extends my screenplay Rise of the Rejects beyond the page — into a cinematic, interactive world that fuses code, creativity, and conviction.",
     image: "/images/rotr-portfolio-logo.png",
     link: "https://riseoftherejects.vercel.app",
   },
